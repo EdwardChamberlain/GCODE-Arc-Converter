e73c16a8b09eba39f6f42620253dbaf0c1fe925e
Edward Chamberlain <edward.chamberlain@me.com>
2020-04-15 00:09:42 +0100

    Cleanup Variable Names

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from alive_progress import alive_bar
 
 import gcode_utils
 
-# IMPORT GCODE
-imported_file, GCODE_FILE = gcode_utils.read_gcode_file("skin.gcode")
+INPUT_FILE = "skin.gcode"
+OUTPUT_FILE = "Output.gcode"
 
-# gcode_utils.plot_gcode(imported_file)
+# IMPORT GCODE
+parsed_file, gcode_file = gcode_utils.read_gcode_file(INPUT_FILE)
 
 # STRIP NON G1 MOVES
 trimmed_data = []
-for i in imported_file:
+for i in parsed_file:
     if i['G'] == 1:
         trimmed_data.append(i)
-gcode_utils.plot_gcode(trimmed_data)
 
 # GET ARC LOCATIONS
 found_arcs = gcode_utils.scan_for_arcs(trimmed_data)
@@ -22,31 +22,26 @@ found_arcs = gcode_utils.scan_for_arcs(trimmed_data)
 print(f"{len(found_arcs)} Arcs found!")
 
 # GENERATE ARCS FOR FOUND ARCS
-for i in found_arcs:
+for arc in found_arcs:
     # GET ARC
-    COMMAND = gcode_utils.compute_arc_move(trimmed_data[i['START']:i['STOP']])
+    COMMAND = gcode_utils.compute_arc_move(trimmed_data[arc['START']:arc['STOP']])
 
     # GET LN NUMBER
-    LINE = {'START':trimmed_data[i['START']]['ln'], 'STOP':trimmed_data[i['STOP']]['ln']}
-    
-    # print(f"i = {i} AT: {LINE}: {COMMAND}")
+    LINE = {'START':trimmed_data[arc['START']]['ln'], 'STOP':trimmed_data[arc['STOP']]['ln']}
 
-    # REPLACE FILE INFO
-    for i in range(LINE['START'], LINE['STOP']):
-        GCODE_FILE[i] = "# CONVERTED TO ARC"
-    GCODE_FILE[LINE['START']] = COMMAND
+    # REposition FILE INFO
+    for position in range(LINE['START'], LINE['STOP']):
+        gcode_file[position] = "# CONVERTED TO ARC"
+    gcode_file[LINE['START']] = COMMAND
 
-OUTPUT_FILE = []
-
-for line in GCODE_FILE:
+# STRIP CONVERTED LINES
+output_list = []
+for line in gcode_file:
     if line != "# CONVERTED TO ARC":
-        OUTPUT_FILE.append(line)
-    
+        output_list.append(line)
 
-print("Arcs Replaced!")
-
-with open('Output.gcode', 'w') as f:
-    for item in OUTPUT_FILE:
+with open(OUTPUT_FILE, 'w') as f:
+    for item in output_list:
         f.write("%s\n" % item)
 
-print(f"Complete! Output created: Output.gcode")
+print(f"Complete! Output created: {OUTPUT_FILE}")
