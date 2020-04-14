9fda9da465b0a4511989abe54f56a1d8bf13c747
Edward Chamberlain <edward.chamberlain@me.com>
2020-04-14 11:01:34 +0100

    imported compute arc move from main.py

--- gcode_utils.py
+++ gcode_utils.py
@@ -1,7 +1,11 @@
 import numpy as np
 from alive_progress import alive_bar
+import circle_fit as cf
+import matplotlib.pyplot as plt
 
 DEBUG = False
+PLOTTING = True
+Sthreshold = 0.06
 
 def parse_gcode(input_string):
     # SET KEYS TO COLLECT 
@@ -17,7 +21,6 @@ def parse_gcode(input_string):
             
     return result
 
-
 def read_gcode_file(filename):
     f = open(filename, 'r')
     GCODE = f.readlines()
@@ -26,26 +29,65 @@ def read_gcode_file(filename):
     coords = []
 
     print(f" Parsing: {filename}:")
-    with alive_bar(len(GCODE)) as bar:  # or a 1000 in the loop example.
+    with alive_bar(len(GCODE)) as bar:
         for i in GCODE:
-            if i[:2] == "G1":
-                
+            if i[:1] == "G":
                 parsed_line = parse_gcode(i)
-                
-                if DEBUG: print(f"{i[:-1]} -> {parsed_line}")
-    
+                if DEBUG: print(f"'{i[:-1]}'' -> {parsed_line}")
                 coords.append(parsed_line)
             bar()
 
     print(f" IMPORTED {len(coords)} points")
     return coords
 
-def move_type(p1, p2):
+def move_type(p1, p2, centre):
 
-    v1 = p1
-    v2 = p2
+    v1 = [p1[0] - centre[0], p1[1] - centre[1]]
+    v2 = [p2[0] - centre[0], p2[1] - centre[1]]
 
     if np.cross(v1, v2) > 0:
         return "G3"
     else: 
         return "G2"
+
+def compute_arc_move(points):
+
+    # PULL X Y COORDS
+    coords = [[s['X'], s['Y']] for s in points] 
+
+    # FIT CIRCLE 
+    xc,yc,r,s = cf.hyper_fit(coords)
+    if DEBUG: print(f"CIRCLE FITTED:\n    X: {xc},\n    Y: {yc},\n    R: {r},\n    S: {s}")
+
+    # DETERMINE MOVE TYPE
+    movetype = move_type(coords[0][:2], coords[1][:2], (xc, yc))
+
+    # SUM E TOTAL
+    E_total = sum([i['E'] for i in points])
+
+    # FORMULATE G CODE COMMAND
+    if s < Sthreshold:
+        print(f'{round(s, 5)} : {movetype} X{coords[-1][0]} Y{coords[-1][1]} R{round(r, 5)} E{round(E_total, 5)}')
+    else:
+        print(f"{round(s, 5)} : GCODE: NOT AN ARC MOVE")
+
+
+    # ------------- PLOTTING --------------
+    # ASK TO PLOT
+    #if input("PLOT FIGURE? (y/n): ") == "y":
+    if PLOTTING:
+        # CREATE FIGURE
+        fig, ax = plt.subplots()
+        ax.set_aspect(1)
+        ax.set_xlim([50,150])
+        ax.set_ylim([50,150])
+
+        # PLOT SCATTER POINTS
+        plt.scatter([i[0] for i in coords], [i[1] for i in coords])
+
+        # PLOT CIRCLE
+        circle1 = plt.Circle((xc, yc), r, linestyle='--', fill=False)
+        ax.add_artist(circle1)
+
+        # SHOW PLOT
+        plt.show()
--- main.py
+++ main.py
@@ -1,48 +1,15 @@
 import circle_fit as cf
-import matplotlib.pyplot as plt
 import numpy as np
+from alive_progress import alive_bar
 
 import gcode_utils
-
-Sthreshold = 0.002
 
 # IMPORT GCODE
 imported_file = gcode_utils.read_gcode_file("Sample.gcode")
 
-# PULL X Y COORDS
-coords = [[s['X'], s['Y']] for s in imported_file] 
+# TRIM GCODE -  Converts into batches. 
+batch_size = 30
 
-# TRIM GCODE - all points is likely too many, so running with less meakes more sense for now!
-coords = coords[0:20] 
-
-# FIT CIRCLE 
-xc,yc,r,s = cf.hyper_fit(coords)
-print(f"CIRCLE FITTED:\n    X: {xc},\n    Y: {yc},\n    R: {r},\n    S: {s}")
-
-# DETERMINE MOVE TYPE
-movetype = gcode_utils.move_type(coords[0][:2], coords[1][:2])
-
-# SUM E TOTAL
-E_total = sum([i['E'] for i in imported_file])
-
-# FORMULATE G CODE COMMAND
-if s < Sthreshold:
-    print(f'GCODE:\n    "{movetype} X{coords[-1][0]} Y{coords[-1][1]} R{round(r, 5)} E{round(E_total, 5)}"')
-else:
-    print(f"GCODE:\n    NOT AN ARC MOVE")
-
-
-# ------------- PLOTTING --------------
-# CREATE FIGURE
-fig, ax = plt.subplots()
-ax.set_aspect(1)
-
-# PLOT SCATTER POINTS
-plt.scatter([i[0] for i in coords], [i[1] for i in coords])
-
-# PLOT CIRCLE
-circle1 = plt.Circle((xc, yc), r, linestyle='--', fill=False)
-ax.add_artist(circle1)
-
-# SHOW PLOT
-plt.show()
+for i in range(0, len(imported_file), batch_size):
+    subList = [i for i in imported_file[i:(i+batch_size)]]
+    gcode_utils.compute_arc_move(subList)
