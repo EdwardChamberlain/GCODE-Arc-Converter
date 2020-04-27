9a6abb1fd08a19243b13e345c3d1ea71a71cb1bb
Edward Chamberlain <edward.chamberlain@me.com>
2020-04-27 18:39:19 +0100

    Final release

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from alive_progress import alive_bar
 
 import gcode_utils as gcu
 
-INPUT_FILE = "gcode/skin.gcode"
+INPUT_FILE = "gcode/SampleModel.gcode"
 OUTPUT_FILE = "Output.gcode"
 PLOTTING = False
 
@@ -25,26 +25,34 @@ if PLOTTING:
     for i in arcs_indexs:
         gcu.plot_gcode_arc(trimmed_data, i['START'], i['STOP'], plot_title="Arc Found!")
 
+with open("test1.gcode", 'w') as f:
+    for item in gcode_file:
+        f.write("%s\n" % item)
+
 # CREATE PROGRESS BAR
 print("Implementing Arcs:")
 with alive_bar(len(arcs_indexs)) as bar:
 
     # CREATE THE GCODE COMMAND FOR THE ARC
     for arc in arcs_indexs:
-        Arc_command = gcu.build_arc_move(trimmed_data[arc['START'] : arc['STOP']+1])
+        Arc_command = gcu.build_arc_move(trimmed_data[arc['START'] : arc['STOP'] + 1])
 
         # GET LINE NUMBER
         start_ln = gcu.get_line_number(trimmed_data[arc['START']])
         end_ln = gcu.get_line_number(trimmed_data[arc['STOP']])
 
         # REPLACE LINES start+1 TO END
-        for line in range(arc['START'] + 1, arc['STOP']):
+        for line in range(start_ln + 1, end_ln + 1):
             gcode_file[line] = "# CONVERTED TO ARC"
 
         # ADD IN ARC
-        gcode_file[arc['START']+1] = Arc_command
+        gcode_file[start_ln+1] = Arc_command
 
         bar()
+
+with open("test2.gcode", 'w') as f:
+    for item in gcode_file:
+        f.write("%s\n" % item)
 
 # TIDY UP
 output_list = []
