d993cef13e7856103cee50df34e6b12bdfc2844e
Edward Chamberlain <edward.chamberlain@me.com>
2020-04-27 18:39:31 +0100

    File to plot gcode

--- plot_gcode_file.py
+++ plot_gcode_file.py
@@ -5,5 +5,8 @@ INPUT_FILE = "Output.gcode"
 # IMPORT GCODE
 parsed_file, gcode_file = gcu.read_gcode_file(INPUT_FILE)
 
+print(parsed_file[3])
+
 # PLOT GCODE
 gcu.plot_gcode(parsed_file, plot_title="Imported Gcode")
+
