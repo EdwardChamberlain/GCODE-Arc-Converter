90783f96356369d77ec702450855a180b4ac5cdc
Edward Chamberlain <edward.chamberlain@me.com>
2020-04-27 18:39:03 +0100

    Added offset, start/end moves, adjusted radius,

--- gen.py
+++ gen.py
@@ -3,20 +3,25 @@ import numpy as np
 
 # SET STARTING POINTS
 coords = [
-    {'G':1, 'X':0, 'Y':-15, 'E':0},
-    {'G':1, 'X':15, 'Y':-15, 'E':0}
+    {'G':1, 'X':0, 'Y':-50, 'E':0},
+    {'G':1, 'X':50, 'Y':-50, 'E':0}
 ]
 
 # CREATE PERFECT ARC MOVE
-r = 15
+r = 50
 for i in range(0, 91, 5):
     y = r * np.sin(np.deg2rad(i))
     x = r * np.cos(np.deg2rad(i))
     coords.append({'G':1, 'X':x, 'Y':y, 'E':0})
 
 # APPEND FINSIHING MOVES
-coords.append({'G':1, 'X':-15, 'Y':15, 'E':0})
-coords.append({'G':1, 'X':-15, 'Y':0, 'E':0})
+coords.append({'G':1, 'X':-50, 'Y':50, 'E':0})
+coords.append({'G':1, 'X':-50, 'Y':0, 'E':0})
+
+# MOVE ORIGIN TO FRONT LEFT CORNER
+for move in coords:
+    move['X'] = move['X']+100
+    move['Y'] = move['Y']+100
 
 # CONVERT TO STRINGS
 output = [gcode_utils.build_g_move(n) for n in coords]
